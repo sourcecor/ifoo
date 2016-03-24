206cc94dccc0ba8b8d7fd19f9c95636032cb2d87
RayChang <phileiny@gmail.com>
2016-03-24 17:41:30 +0800

    change confirmation email  default url

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,7 +78,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   #config.action_mailer.default_url_options = { :host => 'localhost:3000' }
-  config.action_mailer.default_url_options = { :host => 'https://www.air-fitsense.com' }
+  config.action_mailer.default_url_options = { :host => 'www.air-fitsense.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     port:           587,
