f269d44c94431d445d071138ec841376ae541c6b
RayChang <phileiny@gmail.com>
2016-03-24 17:31:33 +0800

    test E-amil-2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,8 +77,8 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
-  #config.action_mailer.default_url_options = { :host => 'https://api.mailgun.net/v3/mg.air-fitsense.com' }
+  #config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.default_url_options = { :host => 'https://www.air-fitsense.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     port:           587,
