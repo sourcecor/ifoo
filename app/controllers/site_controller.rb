4d0b4f9f1d472be4827e888403cd27472f10a1f6
RayChang <phileiny@gmail.com>
2016-05-07 11:24:02 +0800

    test  devise mode

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -1,7 +1,7 @@
 class SiteController < ApplicationController
 
   def index
-    flash[:notice] =   I18n.locale
+    #flash[:notice] =   I18n.locale
   end
 
   def find
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,9 +76,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-
-  #config.action_mailer.default_url_options = { :host => 'localhost:3000' }
   config.action_mailer.default_url_options = { :host => 'www.air-fitsense.com' }
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = config_for(:email).symbolize_keys
+  config.action_mailer.delivery_method = :letter_opener
+  #config.action_mailer.delivery_method = :smtp
+  #config.action_mailer.smtp_settings = config_for(:email).symbolize_keys
 end
