0fb7e5fbaca5d394f211109c92f3a119f863b692
RayChang <phileiny@gmail.com>
2016-05-08 23:49:20 +0800

    creditor test

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,7 +79,6 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
   config.action_mailer.default_url_options = { :host => 'www.air-fitsense.com' }
 
-  #config.action_mailer.delivery_method = :letter_opener
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = config_for(:email).symbolize_keys
 
